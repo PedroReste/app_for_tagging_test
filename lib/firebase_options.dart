7cd4e5973d70171d328bbf35d34bdace7ac43933
Pedro Reste <58362328+PedroReste@users.noreply.github.com>
2026-03-10 10:46:23 -0300

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,24 @@
+// lib/firebase_options.dart
+//
+// ⚠️ ATENÇÃO: Este arquivo é gerado automaticamente pelo FlutterFire CLI.
+// Execute: flutterfire configure
+//
+// NÃO versione este arquivo com credenciais reais.
+// Ele está no .gitignore por segurança.
+//
+// Para gerar este arquivo:
+// dart pub global activate flutterfire_cli
+// flutterfire configure
+
+// O arquivo gerado terá este formato:
+//
+// import 'package:firebase_core/firebase_core.dart'
+// show FirebaseOptions;
+// import 'package:flutter/foundation.dart'
+// show defaultTargetPlatform, kIsWeb, TargetPlatform;
+//
+// class DefaultFirebaseOptions {
+// static FirebaseOptions get currentPlatform { ... }
+// static const FirebaseOptions android = FirebaseOptions(...);
+// static const FirebaseOptions ios = FirebaseOptions(...);
+// }
